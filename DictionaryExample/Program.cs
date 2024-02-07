779172a9a17f5471de339d87f458de6a750c54dd
Vincent Chu <21vincentchu@gmail.com>
2024-02-07 13:18:41 -0600

    Add files via upload

---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Dynamic;
+using System.Formats.Tar;
+
+namespace DictionaryExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Declare a dictionary of student nams and scores. NAme is a string ans score is a float
+        Dictionary<string,float> studentScores = new Dictionary<string, float>(){
+            {"brian", 94},
+            {"cydni", 97.1f},
+            {"Truman", 87.8f}
+        };
+
+        //add an entry to the dictionary
+        studentScores.Add("jamie", 69.8f);
+
+        //print values from the dictionary
+        Console.WriteLine(studentScores["brian"]);
+        Console.WriteLine(studentScores["cydni"]);
+
+        Console.WriteLine("\n\n");
+        //Loop through the dictionary
+        foreach(KeyValuePair<string,float> student in studentScores)
+        {
+            Console.WriteLine($"{student.Key}: {student.Value}");
+        }
+
+        Console.WriteLine("\n\n");
+        foreach(string key in studentScores.Keys)
+        {
+            Console.WriteLine($"{key}: {studentScores[key]}");
+        }
+
+        Console.WriteLine("\n\n");
+        foreach(var student in studentScores)
+        {
+            Console.WriteLine($"{student.Key}: {student.Value}");
+        }
+        
+        //Check if key exists
+        Console.WriteLine("\n\n");
+        String searchKey = "GGGGG";
+
+        if(studentScores.ContainsKey(searchKey))
+        {
+            Console.WriteLine($"{searchKey}; {studentScores[searchKey]}");
+        }else{
+            Console.WriteLine($"{searchKey} does not exist in the dictionary");
+        }
+
+
+    }
+}
---
+++ MenuOrder/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace MenuOrder;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, double> order = new Dictionary<string, double>()
+        {
+            { "Baja Taco", 4.00 },
+            { "Burrito", 7.50 },
+            { "Bowl", 8.50 },
+            { "Nachos", 11.00 },
+            { "Quesadilla", 8.50 },
+            { "Super Burrito", 8.50 },
+            { "Super Quesadilla", 9.50 },
+            { "Taco", 3.00 },
+            { "Tortilla Salad", 8.00 }
+        };
+
+        double orderTotal = 0;
+
+        Boolean keepGoing = true;
+        while(keepGoing)
+        {
+            String end = "end";
+            Console.WriteLine("Item");
+            String input = Console.ReadLine()!;
+
+            foreach(string dictionaryKey in order.Keys)
+            {  
+                if(input.ToLower() == dictionaryKey.ToLower())
+                {
+                    orderTotal += order[dictionaryKey];
+                    Console.WriteLine($"Total: ${orderTotal:F2}" );
+                }
+            }
+            
+            if(String.Equals(end, input, StringComparison.OrdinalIgnoreCase) == true)
+            {
+                Environment.Exit(1);
+            }   
+        }
+    }
+}
